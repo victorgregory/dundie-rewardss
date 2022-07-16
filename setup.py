9221fce152066dc100223859b0601ead4ccf54f8
Victor Gregory <victor.gregory.cavalheiro@gmail.com>
2022-07-16 11:23:23 -0300

    added gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin"
+    author="Victor Gregory"
+    packages=find_packages(),
+
+)
